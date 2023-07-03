d80fb2a56a08d7b84d06c90f2fdc2ea326abe4a5
jadgardner <jadgardner@protonmail.com>
2023-07-03 15:00:42 +0000

    building out ns_reni

---
+++ reni/field_components/film_siren.py
@@ -0,0 +1,81 @@
+# Copyright 2022 The Nerfstudio Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Siren MLP"""
+
+import numpy as np
+import torch
+from torch import nn
+
+def kaiming_leaky_init(m):
+    classname = m.__class__.__name__
+    if classname.find("Linear") != -1:
+        torch.nn.init.kaiming_normal_(m.weight, a=0.2, mode="fan_in", nonlinearity="leaky_relu")
+
+
+def frequency_init(freq):
+    def init(m):
+        with torch.no_grad():
+            if isinstance(m, nn.Linear):
+                num_input = m.weight.size(-1)
+                m.weight.uniform_(-np.sqrt(6 / num_input) / freq, np.sqrt(6 / num_input) / freq)
+
+    return init
+
+
+def first_layer_film_sine_init(m):
+    with torch.no_grad():
+        if isinstance(m, nn.Linear):
+            num_input = m.weight.size(-1)
+            m.weight.uniform_(-1 / num_input, 1 / num_input)
+
+
+class CustomMappingNetwork(nn.Module):
+    def __init__(self, in_features, map_hidden_layers, map_hidden_dim, map_output_dim):
+        super().__init__()
+
+        self.network = []
+
+        for _ in range(map_hidden_layers):
+            self.network.append(nn.Linear(in_features, map_hidden_dim))
+            self.network.append(nn.LeakyReLU(0.2, inplace=True))
+            in_features = map_hidden_dim
+
+        self.network.append(nn.Linear(map_hidden_dim, map_output_dim))
+
+        self.network = nn.Sequential(*self.network)
+
+        self.network.apply(kaiming_leaky_init)
+        with torch.no_grad():
+            self.network[-1].weight *= 0.25
+
+    def forward(self, z):
+        frequencies_offsets = self.network(z)
+        frequencies = frequencies_offsets[..., : torch.div(frequencies_offsets.shape[-1], 2, rounding_mode="floor")]
+        phase_shifts = frequencies_offsets[..., torch.div(frequencies_offsets.shape[-1], 2, rounding_mode="floor") :]
+
+        return frequencies, phase_shifts
+
+
+class FiLMLayer(nn.Module):
+    def __init__(self, input_dim, hidden_dim):
+        super().__init__()
+        self.layer = nn.Linear(input_dim, hidden_dim)
+
+    def forward(self, x, freq, phase_shift):
+        x = self.layer(x)
+        freq = freq.expand_as(x)
+        phase_shift = phase_shift.expand_as(x)
+        return torch.sin(freq * x + phase_shift)
+
---
+++ reni/field_components/siren.py
@@ -0,0 +1,79 @@
+# Copyright 2022 The Nerfstudio Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Siren MLP"""
+
+import numpy as np
+import torch
+from torch import nn
+
+class SineLayer(nn.Module):
+    """
+    Sine layer for the SIREN network.
+    """
+    def __init__(self, in_features, out_features, bias=True, is_first=False, omega_0=30):
+        super().__init__()
+        self.omega_0 = omega_0
+        self.is_first = is_first
+
+        self.in_features = in_features
+        self.linear = nn.Linear(in_features, out_features, bias=bias)
+
+        self.init_weights()
+
+    def init_weights(self):
+        with torch.no_grad():
+            if self.is_first:
+                self.linear.weight.uniform_(-1 / self.in_features, 1 / self.in_features)
+            else:
+                self.linear.weight.uniform_(
+                    -np.sqrt(6 / self.in_features) / self.omega_0,
+                    np.sqrt(6 / self.in_features) / self.omega_0,
+                )
+
+    def forward(self, x):
+        return torch.sin(self.omega_0 * self.linear(x))
+    
+
+class Siren(nn.Module):
+    """Siren network.
+
+    Args:
+        in_dim: Input layer dimension
+        num_layers: Number of network layers
+        layer_width: Width of each MLP layer
+        out_dim: Output layer dimension. Uses layer_width if None.
+        activation: intermediate layer activation function.
+        out_activation: output activation function.
+    """
+
+    def __init__(
+        self,
+        in_dim: int,
+        num_layers: int,
+        layer_width: int,
+        out_dim: Optional[int] = None,
+        activation: Optional[nn.Module] = nn.ReLU(),
+        out_activation: Optional[nn.Module] = None,
+    ) -> None:
+        super().__init__()
+        self.in_dim = in_dim
+        assert self.in_dim > 0
+        self.out_dim = out_dim if out_dim is not None else layer_width
+        self.num_layers = num_layers
+        self.layer_width = layer_width
+        self.activation = activation
+        self.out_activation = out_activation
+        self.net = None
+        self.build_nn_modules()
